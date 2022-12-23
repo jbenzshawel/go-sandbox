112fa7d279c04c67ab08cc51dfc2b527118ae06d
Addison Benzshawel <jbenzshawel@gmail.com>
2022-12-23 15:53:16 -0600

    Setup keycloak container

--- docker/postgres/init-db.sh
+++ docker/postgres/init-db.sh
@@ -2,9 +2,18 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER keycloak;
+	ALTER USER keycloak WITH PASSWORD 'dockerdbpw';
+	CREATE DATABASE keycloak;
+	GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+	\connect keycloak;
+	GRANT ALL PRIVILEGES ON SCHEMA public TO keycloak;
+	\connect postgres;
 	CREATE USER identity;
+	ALTER USER identity WITH PASSWORD 'dockerdbpw';
 	CREATE DATABASE identity;
 	GRANT ALL PRIVILEGES ON DATABASE identity TO identity;
 	\connect identity;
     CREATE SCHEMA identity;
+	\connect postgres;
 EOSQL
