e1ff8b2d02c6886c548897794e69c4472bac350b
Addison Benzshawel <jbenzshawel@gmail.com>
2023-01-28 17:59:59 -0600

    wait until keycloak is up before starting service

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+while true
+ do
+    if [[ $(curl -sL -w "%{http_code}\\n" "keycloak:8080" -o /dev/null) == "200" ]]; then
+        break
+    else
+        echo "Waiting for keycloak availability..."
+        sleep 0.5
+    fi
+done
+
 go run .
